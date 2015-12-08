280258a860e61034d55d8062254500c6b60269f9
Kurt <Kurt@10.4.2.33>
2015-12-08 09:44:08 -0600

    Helper Code

---
+++ ShoppingList/src/shoppinglist/ShoppingList.java
@@ -0,0 +1,16 @@
+package shoppinglist;
+
+/**
+ *
+ * @author change to your name or loose 5 points
+ */
+public class ShoppingList {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // code application logic here
+    }
+    
+}
